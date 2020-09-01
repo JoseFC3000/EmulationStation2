f09e6a6ccf694ffd6b8d9a80e594d75d8c7ffbab
JoseFC3000 <70484443+JoseFC3000@users.noreply.github.com>
2020-09-01 05:48:17 +0100

    Update FileSorts.h

--- es-app/src/FileSorts.h
+++ es-app/src/FileSorts.h
@@ -16,6 +16,7 @@ namespace FileSorts
 	bool compareGenre(const FileData* file1, const FileData* file2);
 	bool compareDeveloper(const FileData* file1, const FileData* file2);
 	bool comparePublisher(const FileData* file1, const FileData* file2);
+	bool compareKylton(const FileData* file1, const FileData* file2);
 	bool compareSystem(const FileData* file1, const FileData* file2);
 
 	extern const std::vector<FileData::SortType> SortTypes;
